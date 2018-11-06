b1e655e16a10b66efc44557b1ebb562b6fdf4b8d
wingkwong <wingkwong.code@gmail.com>
2018-11-07 00:28:39 +0800

    #12 #13 added missing lib & set required to email & fixed typo

--- server/models/User.js
+++ server/models/User.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const crypto = require('crypto');
+const jwt = require('jsonwebtoken');
 
 const Schema = mongoose.Schema;
 
@@ -6,7 +8,7 @@ const UsersSchema = new Schema({
     uuid : { type: String, required: true, unique: true },
     username: { type: String, required: true },
     password: { type: String, required: true },
-    email: { type: String },
+    email: { type: String, required: true },
     salt: { type: String },
     createdAt: {
         type: Date,
@@ -28,13 +30,12 @@ UsersSchema.methods.setPassword = function(password) {
     const today = new Date();
     const expirationDate = new Date(today);
     expirationDate.setDate(today.getDate() + 60);
-  
     return jwt.sign({
       username: this.username,
       email: this.email,
       id: this.uuid,
       exp: parseInt(expirationDate.getTime() / 1000, 10),
-    }, process.env.JWTSecret);
+    }, process.env.JWT_SECRET);
   }
   
   UsersSchema.methods.toAuthJSON = function() {
