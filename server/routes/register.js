27d35177dabd948143de57e93bfc142854fa084f
wingkwong <wingkwong.code@gmail.com>
2018-11-07 00:28:58 +0800

    #12 added validateRegistration

--- server/routes/register.js
+++ server/routes/register.js
@@ -19,7 +19,7 @@ module.exports = function (server) {
             endResponseInAction: false,
 
             beforeActions: [{
-                middlewares: [uuidGenerator]
+                middlewares: [uuidGenerator, validateRegistration]
             }],
             // actions: {}, // list (GET), create (POST), read (GET), update (PUT), delete (DELETE)
             afterActions: [
@@ -31,6 +31,65 @@ module.exports = function (server) {
     function uuidGenerator(req, res, next) {
         var id = uuidv4();
         req.body.uuid = id;
+        next();
+    }
+
+    function validateRegistration(req, res, next) {
+        let username = req.body.username;
+        let password = req.body.password;
+        let confirmPassword = req.body.confirmPassword;
+        let email = req.body.email;
+        let uuid = req.body.uuid;
+
+        if(!username) {
+            return res.status(422).json({
+                error: 'Username is required'
+            })
+        }
+
+        if(!password) {
+            return res.status(422).json({
+                error: 'password is required'
+            })
+        }
+
+        if(!confirmPassword) {
+            return res.status(422).json({
+                error: 'confirmPassword is required'
+            })
+        }
+
+        if(!email) {
+            return res.status(422).json({
+                error: 'email is required'
+            })
+        }
+        
+        if(password !== confirmPassword) {
+            return res.status(400).json({
+                error: 'Passwords do not match'
+            })
+        }
+
+        User.findOne( {username} )
+            .then( (user) => {
+                if(!user) {
+                    const newUser = new User({
+                        uuid,
+                        username,
+                        email
+                    });
+
+                    newUser.setPassword(password);
+                    return newUser.save()
+                        .then(() => res.json({
+                            user: newUser.toAuthJSON()
+                        }))
+                }
+                return res.status(400).json({
+                    error: 'Failed to register'
+                })
+            })
     }
 
     function modifyQueryResult (req, res) {
