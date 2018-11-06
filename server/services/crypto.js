4c008f9694c28073b3d793457b3301a652174bb5
wingkwong <wingkwong.code@gmail.com>
2018-11-06 21:05:03 +0800

    #20 crypto module

---
+++ server/services/crypto.js
@@ -0,0 +1,26 @@
+const crypto = require('crypto');
+const md5 = require('md5');
+
+function encrypt(content) {
+  let iv_length = 16;
+  let iv = crypto.randomBytes(iv_length);
+  let cipher = crypto.createCipheriv('AES-256-CBC', Buffer.from(md5(process.env.CRYPTO_PASSPHRASE)), iv);
+  let encrypted = cipher.update(content);
+  encrypted = Buffer.concat([encrypted, cipher.final()]);
+  return iv.toString('hex') + ':' + encrypted.toString('hex');
+}
+
+function decrypt(content){
+  let textParts = content.split(':');
+  let iv = Buffer.from(textParts.shift(), 'HEX');
+  let encryptedContent = Buffer.from(textParts.join(':'), 'HEX');
+  let decipher = crypto.createDecipheriv('AES-256-CBC', Buffer.from(md5(process.env.CRYPTO_PASSPHRASE)), iv);
+  let decrypted = decipher.update(encryptedContent);
+  decrypted = Buffer.concat([decrypted, decipher.final()]);
+  return decrypted.toString();
+}
+
+module.exports = {
+	encrypt,
+	decrypt
+}
