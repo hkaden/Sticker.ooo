6154d0eade68ddc8500281eb3431927850902024
Kaden <info@hkaden.me>
2018-11-13 20:48:42 +0800

    #12 added register ui

---
+++ pages/register.js
@@ -0,0 +1,14 @@
+import Layout from '../components/Layout/Layout'
+import RegisterForm from '../components/RegisterForm/RegisterForm'
+
+export default class Register extends React.Component {
+
+  render() {
+
+    return (
+      <Layout>
+        <RegisterForm/>
+      </Layout>
+    );
+  }
+}
