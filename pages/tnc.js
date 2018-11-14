c63546c7f2c58e6ec9b4c9e1d65a243ccb8e8057
Kaden <info@hkaden.me>
2018-11-14 16:40:02 +0800

    #129 added tnc page

---
+++ pages/tnc.js
@@ -0,0 +1,14 @@
+import Layout from '../components/Layout/Layout'
+import Tnc from '../components/Tnc/Tnc'
+
+export default class TNC extends React.Component {
+
+  render() {
+
+    return (
+      <Layout>
+        <Tnc/>
+      </Layout>
+    );
+  }
+}
