1e312722dd84c0aa4067413c6a4684696e0c0f8d
wingkwong <wingkwong.code@gmail.com>
2018-11-14 22:02:37 +0800

    #112 added forget password route

---
+++ server/routes/forgetPassword.js
@@ -0,0 +1,61 @@
+
+
+const { body } = require('express-validator/check');
+const { sanitizeBody } = require('express-validator/filter');
+const User = require('../models/User');
+const Token = require('../models/Token');
+const auth = require('../middleware/auth');
+const { expressValidatorErrorHandler } = require('../utils/expressErrorHandlers');
+const { sendEmail } = require('../utils/nodeMailer');
+const { TYPES, MESSAGES } = require('../configs/constants');
+
+module.exports = function (server) {
+  // Docs: https://github.com/ryo718/mongoose-crudify
+  server.post(
+    '/api/forgetPassword',
+    auth.optional,
+    [
+      body('email').withMessage(MESSAGES.IS_REQUIRE),
+      body('email').isEmail(),
+      // sanitizeBody('email').normalizeEmail({ remove_dots: false }),
+      expressValidatorErrorHandler,
+    ],
+    async (req, res, next) => {
+      try {
+        const { email } = req.body;
+        await User.findOne({ email }, (err, user) => {
+          console.log("hrer")
+          if (!user || !user.isVerified) {
+            return res.status(400).json({
+              type: TYPES.INVALID_USER,
+              message: MESSAGES.FAILED_TO_MATCH_USER,
+            });
+          }
+          console.log("hrer2")
+          const token = new Token({
+            uuid: user.uuid,
+            type: TYPES.FORGET_PASSWORD
+          });
+
+          token.setToken(email);
+
+          let subject = 'Forget Password';
+          let content = `${'Hello,\n\n' + 'You recently have requested to reset password. Please do it by clicking the link: \nhttp:\/\/'}${req.headers.host}\/resetPassword\/${token.token}.\n`;
+          let successReturn = {
+            type: TYPES.RESET_PASSWORD_EMAIL_SENT,
+            message: MESSAGES.RESET_PASSWORD_EMAIL_SENT_SUCCESS + email
+          };
+          let failedReturn = {
+            type: TYPES.FAILED_TO_SEND_RESET_PASSWORD_EMAIL,
+            message: MESSAGES.FAILED_TO_SEND_RESET_PASSWORD_EMAIL,
+          };
+          console.log("hrer222")
+          sendEmail(email, subject, content, req, res, successReturn, failedReturn);
+          console.log("hrer3")
+        });
+      } catch (e) {
+        next(e);
+      }
+    },
+  );
+};
