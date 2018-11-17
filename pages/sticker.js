6350c42eef1e9b2f9993b355295892ab06071365
lckj210 <lckj210@gmail.com>
2018-11-17 18:05:18 +0800

    Revert "#202 Add Open in Twemoji"
    This reverts commit 5528ac86a6e5c5546db349779b7bde5886edbcd2.

--- pages/sticker.js
+++ pages/sticker.js
@@ -46,10 +46,6 @@ class StickerPage extends Component {
       return this.props.userAgent.includes('Chrome') || this.props.userAgent.includes('Android');
     }
 
-    isMobile() {
-      return this.props.userAgent.includes('iPhone');
-    }
-
     render() {
       const { stickersList } = this.props;
       const packList = stickersList[0].stickers.map((pack, index) => (
@@ -58,15 +54,14 @@ class StickerPage extends Component {
           title={`Pack ${index + 1}`}
           extra={<Button style={{ marginLeft: '10px' }} type="primary" icon="plus" size="large" ghost onClick={() => {
             let url = `/api/stickers/${this.props.uuid}/packs/${index + 1}.json`;
-            if (this.isMobile()) {
-              window.location.href = `twesticker://json?urlString=${window.location.origin}${url}`
+            if (this.props.userAgent.includes('iPhone')) {
+              Router.push(url);
             } else {
               saveAs(url, `${stickersList[0].name}_${index + 1}.json`, {type: 'application/json'});
             }
+
           }} >
-            {
-              this.isMobile() ? 'Open in Twemoji' : 'Download JSON'
-            }
+            Download JSON
           </Button>}
         >
           {
