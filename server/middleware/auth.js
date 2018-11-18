d85da8be421bbd659afa513a732e1a36167fcfa0
wingkwong <wingkwong.code@gmail.com>
2018-11-18 12:08:55 +0800

    #227 added middleware to check if users r admin

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,6 +1,8 @@
 const jwt = require('express-jwt');
 const _ = require('lodash');
 const fs = require('fs');
+const User = require('../models/User');
+const { TYPES, MESSAGES } = require('../configs/constants');
 
 // const getTokenFromHeaders = (req) => {
 //   const { headers: { authorization } } = req;
@@ -33,6 +35,34 @@ const auth = {
     credentialsRequired: false,
     algorithm: 'RS256',
   }),
+  requiredAdmin: (req, res, next) => {
+    const cookie = req.cookies.jwtToken;
+    if (cookie == undefined) {
+      return res.status(401).json({
+        type: TYPES.UNAUTHORIZED_ACTION,
+        message: MESSAGES.UNAUTHORIZED_ACTION,
+      })
+    } else {
+      const user = new User();
+      const payload = user.verifyJWT(cookie);
+      if(payload == null) {
+        return res.status(400).json({
+          type: TYPES.FAILED_TO_VERIFY_JWT_TOKEN,
+          message: MESSAGES.FAILED_TO_VERIFY_JWT_TOKEN,
+        })
+      } else {
+        console.log(payload)
+        if(payload.role && payload.role == 'admin'){
+          next()
+        } else {
+          return res.status(401).json({
+            type: TYPES.UNAUTHORIZED_ACTION,
+            message: MESSAGES.UNAUTHORIZED_ACTION,
+          })
+        }
+      }
+    }
+  },
   getUserUUID: req => _.get(req, 'payload.uuid', null),
 };
 
