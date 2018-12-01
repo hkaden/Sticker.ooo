223e75b9ba1b89c4b9cb3e84678225d7a08189d7
wingkwong <wingkwong.code@gmail.com>
2018-12-01 11:23:23 +0800

    temp fix - hide auth.optional

--- server/routes/stickerooo.js
+++ server/routes/stickerooo.js
@@ -11,7 +11,7 @@ const { requestParameterValidator } = require('../utils/validators');
 module.exports = function (server) {
   server.get(
     '/api/stickerooo',
-    auth.optional,
+    //auth.optional,
     async (req, res, next) => {
       try {
         let query = Stickerooo
@@ -42,7 +42,7 @@ module.exports = function (server) {
   //FIXME: use the commented out instead after beta
   server.post(
     '/api/stickerooo',
-    auth.optional,
+    //auth.optional,
     async (req, res, next) => {
       try {
         let query = Stickerooo
